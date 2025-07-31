f9134c19f815869bc8de8a23c7b0c863b75e028b
Luciano Morais <lucianopythonista@gmail.com>
2025-07-31 14:20:37 -0300

    feat: nav

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
 import Dashboard from './components/dashboard/Dashboard.vue';
+import Navegacao from './components/navegacao/Navegacao.vue';
 </script>
 
 <template>
-  <Dashboard />
+  <header>
+    <Navegacao />
+  </header>
+
+  <main>
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div class="flex justify-center">
+          <Dashboard />
+        </div>
+    </div>
+  </main>
 </template>
 
 <style scoped></style>
--- frontend/src/components/dashboard/listapedidos/ListaPedidos.vue
+++ frontend/src/components/dashboard/listapedidos/ListaPedidos.vue
@@ -1,6 +1,26 @@
 <script lang="ts" setup>
+import { ref } from 'vue';
+
+const pedidos = ref([
+    { id: 1, pedido: 'Viagem teste 1' },
+    { id: 2, pedido: 'Viagem teste 2' },
+    { id: 3, pedido: 'Viagem teste 3' }
+]);
 </script>
 
 <template>
-
+    <table class="table-auto w-full">
+        <thead>
+            <tr>
+                <th class="border px-4 py-2">ID</th>
+                <th class="border px-4 py-2">Pedido</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="pedido in pedidos" :key="pedido.id">
+                <td class="border px-4 py-2">{{ pedido.id }}</td>
+                <td class="border px-4 py-2">{{ pedido.pedido }}</td>
+            </tr>
+        </tbody>
+    </table>
 </template>
