55739716b87755e7d0e8f407247e6e4d2e8098ac
Luciano Morais <lucianopythonista@gmail.com>
2025-08-01 15:05:16 -0300

    dropdown

--- frontend/src/components/ListaPedidos.vue
+++ frontend/src/components/ListaPedidos.vue
@@ -1,10 +1,32 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
+import Acao from './Acao.vue';
 
 const pedidos = ref([
-    { id: 1, pedido: 'Viagem teste 1' },
-    { id: 2, pedido: 'Viagem teste 2' },
-    { id: 3, pedido: 'Viagem teste 3' }
+    {
+        id: 1,
+        destino: 'São Paulo',
+        dataIda: '2025-08-01',
+        dataVolta: '2025-08-05',
+        solicitante: 'Kowalski',
+        status: 'Solicitado'
+    },
+    {
+        id: 2,
+        destino: 'Distrito Federal',
+        dataIda: '2025-08-01',
+        dataVolta: '2025-08-05',
+        solicitante: 'Michelangelo',
+        status: 'Aprovado'
+    },
+    {
+        id: 3,
+        destino: 'Rio de Janeiro',
+        dataIda: '2025-08-01',
+        dataVolta: '2025-08-05',
+        solicitante: 'Luciano',
+        status: 'Cancelado'
+    }
 ]);
 </script>
 
@@ -13,13 +35,25 @@ const pedidos = ref([
         <thead>
             <tr>
                 <th class="border px-4 py-2">ID</th>
-                <th class="border px-4 py-2">Pedido</th>
+                <th class="border px-4 py-2">Destino</th>
+                <th class="border px-4 py-2">Ida</th>
+                <th class="border px-4 py-2">Volta</th>
+                <th class="border px-4 py-2">Solicitante</th>
+                <th class="border px-4 py-2">Status</th>
+                <th class="border px-4 py-2">Menu</th>
             </tr>
         </thead>
         <tbody>
             <tr v-for="pedido in pedidos" :key="pedido.id">
                 <td class="border px-4 py-2">{{ pedido.id }}</td>
-                <td class="border px-4 py-2">{{ pedido.pedido }}</td>
+                <td class="border px-4 py-2">{{ pedido.destino }}</td>
+                <td class="border px-4 py-2">{{ pedido.dataIda }}</td>
+                <td class="border px-4 py-2">{{ pedido.dataVolta }}</td>
+                <td class="border px-4 py-2">{{ pedido.solicitante }}</td>
+                <td class="border px-4 py-2">{{ pedido.status }}</td>
+                <td class="border px-4 py-2 text-center">
+                    <Acao />
+                </td>
             </tr>
         </tbody>
     </table>
