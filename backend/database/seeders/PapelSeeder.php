3fde7e3a434a84af8b9a0b624009c442005a2355
Luciano Morais <lucianopythonista@gmail.com>
2025-08-02 18:13:58 -0300

    seeds

---
+++ backend/database/seeders/PapelSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+
+class PapelSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('papel')->insert([
+            'permissao' => 'admin',
+        ]);
+
+        DB::table('papel')->insert([
+            'permissao' => 'usuario',
+        ]);
+    }
+}
---
+++ backend/database/seeders/UsuarioSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Seeder;
+
+class UsuarioSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('usuario')->insert([
+            'idPapel' => 1,
+            'nome' => 'Admin Teste',
+            'email' => 'admin.teste@example.com',
+            'usuario' => 'admin.teste',
+            'senha' => Hash::make('senha123'),
+        ]);
+
+        DB::table('usuario')->insert([
+            'idPapel' => 2,
+            'nome' => 'Usuario Teste',
+            'email' => 'usuario.teste@example.com',
+            'usuario' => 'usuario.teste',
+            'senha' => Hash::make('senha123'),
+        ]);
+    }
+}
