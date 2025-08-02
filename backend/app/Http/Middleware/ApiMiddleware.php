a2b6af3e19e7a10936b8eb0d806a82c02adf9a32
Luciano Morais <lucianopythonista@gmail.com>
2025-08-02 17:49:41 -0300

    middleware-json

---
+++ backend/app/Http/Middleware/ApiMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ApiMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (!$request->isJson()) {
+            return response()->json(['message' => 'Only JSON requests are accepted.'], 406);
+        }
+
+        return $next($request);
+    }
+}
