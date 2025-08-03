b26b598e6397419ae930ba00ec88a31f4b5e22c3
Luciano Morais <lucianopythonista@gmail.com>
2025-08-02 21:40:20 -0300

    rotas

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PedidoController;
+use App\Http\Controllers\UsuarioController;
+use App\Http\Middleware\ApiMiddleware;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/pedido', [PedidoController::class, 'index']);
+Route::get('/usuario', [UsuarioController::class, 'index']);
+
+Route::middleware(ApiMiddleware::class)->group(function () {
+    Route::get('/pedido/{id}', [PedidoController::class, 'show']);
+    Route::post('/pedido', [PedidoController::class, 'store']);
+    Route::put('/pedido/{id}', [PedidoController::class, 'update']);
+
+    Route::get('/usuario/{id}', [UsuarioController::class, 'show']);
+    Route::post('/usuario', [UsuarioController::class, 'store']);
+    Route::put('/usuario/{id}', [UsuarioController::class, 'update']);
 });
