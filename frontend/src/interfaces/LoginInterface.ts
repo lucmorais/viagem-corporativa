b258224ed5af55ea476448697cef8ccbe37b7683
Luciano Morais <lucianopythonista@gmail.com>
2025-08-03 14:48:21 -0300

    interfaces

---
+++ frontend/src/interfaces/LoginInterface.ts
@@ -0,0 +1,4 @@
+export default interface LoginI {
+    usuario: string;
+    senha: string;
+}
---
+++ frontend/src/interfaces/PedidoInterface.ts
@@ -0,0 +1,10 @@
+export default interface PedidoI {
+    id: number;
+    idUsuario: number;
+    destino: string;
+    dataIda: string;
+    dataVolta: string;
+    status: string;
+    created_at: string;
+    updated_at: string;
+}
