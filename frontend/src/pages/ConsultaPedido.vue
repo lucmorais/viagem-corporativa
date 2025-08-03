1d50b0d095c051ab40f526884a51d195e0f9b818
Luciano Morais <lucianopythonista@gmail.com>
2025-08-03 14:49:01 -0300

    ajuste-navegacao

--- frontend/src/pages/ConsultaPedido.vue
+++ frontend/src/pages/ConsultaPedido.vue
@@ -1,14 +1,22 @@
 <script lang="ts" setup>
+import Nav from '@/components/Nav.vue';
 import DetalhePedido from '@/components/DetalhePedido.vue';
 </script>
 
 <template>
-    <section class="rounded-lg overflow-hidden shadow-lg bg-white w-full mt-6">
-        <div class="px-6 py-6">
-            <div class="font-bold text-xl mb-6">Pedido</div>
-            <div>
-                <DetalhePedido />
+    <Nav />
+    <div class="bg-gray-100 min-h-screen">
+        <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div class="flex justify-center">
+                <section class="rounded-lg overflow-hidden shadow-lg bg-white w-full mt-6">
+                    <div class="px-6 py-6">
+                        <div class="font-bold text-xl text-left mb-6">Pedido</div>
+                        <div>
+                            <DetalhePedido />
+                        </div>
+                    </div>
+                </section>
             </div>
         </div>
-    </section>
+    </div>
 </template>
--- frontend/src/pages/Dashboard.vue
+++ frontend/src/pages/Dashboard.vue
@@ -1,14 +1,22 @@
 <script lang="ts" setup>
+import Nav from '@/components/Nav.vue';
 import ListaPedidos from '../components/ListaPedidos.vue';
 </script>
 
 <template>
-  <section class="rounded-lg overflow-hidden shadow-lg bg-white w-full mt-6">
-    <div class="px-6 py-6">
-      <div class="font-bold text-xl text-left mb-6">Pedidos de viagem</div>
-      <div>
-        <ListaPedidos />
+  <Nav />
+  <div class="bg-gray-100 min-h-screen">
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div class="flex justify-center">
+        <section class="rounded-lg overflow-hidden shadow-lg bg-white w-full mt-6">
+          <div class="px-6 py-6">
+            <div class="font-bold text-xl text-left mb-6">Pedidos de viagem</div>
+            <div>
+              <ListaPedidos />
+            </div>
+          </div>
+        </section>
       </div>
     </div>
-  </section>
+  </div>
 </template>
