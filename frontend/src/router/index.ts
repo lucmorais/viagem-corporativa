4b1ff7bf30dc3d539ac7cc9bdb163c3f5ae785d0
Luciano Morais <lucianopythonista@gmail.com>
2025-08-03 14:50:06 -0300

    token

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router';
 import Dashboard from '../pages/Dashboard.vue';
+import Login from '@/pages/Login.vue';
 import CadastroPedido from '../pages/CadastroPedido.vue';
 import ConsultaPedido from '../pages/ConsultaPedido.vue';
 
 const routes: RouteRecordRaw[] = [
   { path: '/', component: Dashboard },
+  { path: '/login', component: Login },
   { path: '/pedido/cadastrar', component: CadastroPedido },
   { path: '/pedido/consultar', component: ConsultaPedido },
 ];
@@ -12,6 +14,16 @@ const routes: RouteRecordRaw[] = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  const autenticado = !!localStorage.getItem('token');
+
+  if (to.path !== '/login' && !autenticado) {
+    next('/login');
+  } else {
+    next();
+  }
 });
 
 export default router;
