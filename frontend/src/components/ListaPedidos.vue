68ef5e6fa4e3f11b9497451382f63f7c8458786b
Luciano Morais <lucianopythonista@gmail.com>
2025-08-03 14:50:39 -0300

    integracao-pedidos

--- frontend/src/components/ListaPedidos.vue
+++ frontend/src/components/ListaPedidos.vue
@@ -1,33 +1,15 @@
 <script lang="ts" setup>
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import Acao from './Acao.vue';
+import { PedidoService } from '../services/PedidoService';
+import type PedidoI from '../interfaces/PedidoInterface';
 
-const pedidos = ref([
-    {
-        id: 1,
-        destino: 'São Paulo',
-        dataIda: '2025-08-01',
-        dataVolta: '2025-08-05',
-        solicitante: 'Kowalski',
-        status: 'Solicitado'
-    },
-    {
-        id: 2,
-        destino: 'Distrito Federal',
-        dataIda: '2025-08-01',
-        dataVolta: '2025-08-05',
-        solicitante: 'Michelangelo',
-        status: 'Aprovado'
-    },
-    {
-        id: 3,
-        destino: 'Rio de Janeiro',
-        dataIda: '2025-08-01',
-        dataVolta: '2025-08-05',
-        solicitante: 'Luciano',
-        status: 'Cancelado'
-    }
-]);
+const pedidos = ref<PedidoI[] | undefined>([]);
+const pedidoService = new PedidoService();
+
+onMounted(async () => {
+    pedidos.value = await pedidoService.getPedidos();
+});
 </script>
 
 <template>
@@ -36,10 +18,10 @@ const pedidos = ref([
             <thead>
                 <tr>
                     <th class="border px-4 py-2">ID</th>
+                    <th class="border px-4 py-2">Solicitante</th>
                     <th class="border px-4 py-2">Destino</th>
                     <th class="border px-4 py-2">Ida</th>
                     <th class="border px-4 py-2">Volta</th>
-                    <th class="border px-4 py-2">Solicitante</th>
                     <th class="border px-4 py-2">Status</th>
                     <th class="border px-4 py-2">Ação</th>
                 </tr>
@@ -47,10 +29,10 @@ const pedidos = ref([
             <tbody>
                 <tr v-for="pedido in pedidos" :key="pedido.id">
                     <td class="border px-4 py-2">{{ pedido.id }}</td>
+                    <td class="border px-4 py-2">{{ pedido.idUsuario }}</td>
                     <td class="border px-4 py-2 truncate">{{ pedido.destino }}</td>
                     <td class="border px-4 py-2 text-center">{{ pedido.dataIda }}</td>
                     <td class="border px-4 py-2 text-center">{{ pedido.dataVolta }}</td>
-                    <td class="border px-4 py-2">{{ pedido.solicitante }}</td>
                     <td class="border px-4 py-2 text-center">{{ pedido.status }}</td>
                     <td class="border px-4 py-2 text-center flex justify-center space-x-2">
                         <Acao :id="pedido.id" :acao="'aprovar'" />
